fa6fbc4450505d6b053012aee2cd3a8a91580820
EC2 Default User <ec2-user@ip-172-31-82-224.ec2.internal>
2022-02-27 07:31:01 +0000

    function for model & controller comm.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,16 @@
 class Movie < ActiveRecord::Base
+    def self.with_ratings(ratings)
+        #returns the entire database of movies if no filters are applied
+        return Movie.all unless ratings
+        #creates a map of ratings as the key and movies as the values
+        ratings = ratings.map { |rating| rating.upcase }
+        #condition to restrict database query
+        Movie.where(rating: ratings)
+    end
+    
+    def self.all_ratings
+        Movie.select(:rating).uniq.map do |record|
+            record.rating
+        end
+    end
 end
